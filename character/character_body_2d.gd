30fd6411070c41251e5d6bcc8def0e79d4dde3bc
Matysso <152792856+Matysso@users.noreply.github.com>
2024-03-14 10:06:15 +0100

    jump changement

--- character/character_body_2d.gd
+++ character/character_body_2d.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 250.0
 const JUMP_VELOCITY = -800.0
+var jump_charge_time = 2.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var sprite_2d = $Sprite2D
 
@@ -13,6 +14,11 @@ func _physics_process(delta):
 		
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		velocity.x = velocity.x
+	
+	
+		
+		
 	
 
 		
@@ -34,11 +40,11 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var horizontal_direction = Input.get_axis("left", "right")
-	
-	if horizontal_direction:
-		velocity.x = horizontal_direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, 30)
+	if is_on_floor():
+		if horizontal_direction:
+			velocity.x = horizontal_direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, 30)
 
 		
 		
